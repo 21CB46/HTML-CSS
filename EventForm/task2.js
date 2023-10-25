28207bfbc5664bdf69ded31923376a9c07f42bcb
Your Name~kunal-511 <yoyokvunal@gmail.com>
2023-10-25 22:13:36 +0530

    Added A event form

---
+++ EventForm/task2.js
@@ -0,0 +1,22 @@
+// function handleFormSubmit(event) {
+//     event.preventDefault
+//     var event = document.getElementById("event").value;
+
+
+// }
+// function checkPhone(){
+//     var phone = document,getElementById("phone").valueOf;
+//     var regex= /^\d{10}$/;
+//     if(regex.test(phone)){
+//         alert("Valid phone number");
+//         else{
+//             alert("Invalid phone number")
+//         }
+//     }
+// }
+
+// var form = document.getElementById("form");
+// form.addEventListener("submit", handleFormSubmit);
+
+
+
